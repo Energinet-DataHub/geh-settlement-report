356996d65474ba1cb9c2652f56184bf88e7e71b6
Vitalie Belinschi <XVIBE@energinet.dk>
2024-09-12 11:56:35 +0200

    remove default value for GridAreas

--- source/settlement-report/DatabaseMigration/Scripts/202409121100_Add_FilterProperties_to_SettlementReport.sql
+++ source/settlement-report/DatabaseMigration/Scripts/202409121100_Add_FilterProperties_to_SettlementReport.sql
@@ -7,5 +7,9 @@ ADD [IncludeMonthlyAmount] [bit] NOT NULL DEFAULT(0);
 GO
 
 ALTER TABLE [settlementreports].[SettlementReport]
-ADD [GridAreas] [nvarchar](2048) NOT NULL DEFAULT('');
+ADD [GridAreas] [nvarchar](2048) NOT NULL CONSTRAINT SettlementReport_GridAreas_Default_Constraint DEFAULT('');
+GO
+
+ALTER TABLE [settlementreports].[SettlementReport]
+DROP CONSTRAINT SettlementReport_GridAreas_Default_Constraint 
 GO
