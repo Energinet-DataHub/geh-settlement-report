4caf361d1a2fd34de13036da554e3c5e4321b85f
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-09-18 16:35:20 +0200

    Update DatabricksSqlQueryBuilder.cs

--- source/settlement-report/SettlementReports.Infrastructure/Experimental/DatabricksSqlQueryBuilder.cs
+++ source/settlement-report/SettlementReports.Infrastructure/Experimental/DatabricksSqlQueryBuilder.cs
@@ -106,7 +106,7 @@ public sealed class DatabricksSqlQueryBuilder
         sqlParameters = sqlParams;
         var translated = TranslateTransactToAnsi(sqlStatement);
         var logString = $"Translated SQL: {translated}" + Environment.NewLine + string.Join(Environment.NewLine, sqlParams.Select(x => $"Parameter Name: {x.Key} | Value: {x.Value}"));
-        _logger ??= _context.GetService<ILoggerFactory>().CreateLogger<DatabricksSqlQueryBuilder>();
+        _logger = _context.GetService<ILoggerFactory>().CreateLogger<DatabricksSqlQueryBuilder>();
         _logger.LogError(logString);
         return translated;
     }
