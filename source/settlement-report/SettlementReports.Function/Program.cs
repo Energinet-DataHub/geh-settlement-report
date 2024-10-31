6ee676c70dd0b3e1fdece3b91bb599685b2cd28b
Jesper Justesen <jesper.justesen@gmail.com>
2024-10-31 14:35:30 +0100

    fixing registrations

--- source/settlement-report/SettlementReports.Function/Program.cs
+++ source/settlement-report/SettlementReports.Function/Program.cs
@@ -15,11 +15,13 @@
 using Energinet.DataHub.Core.App.Common.Extensions.DependencyInjection;
 using Energinet.DataHub.Core.App.FunctionApp.Extensions.Builder;
 using Energinet.DataHub.Core.App.FunctionApp.Extensions.DependencyInjection;
+using Energinet.DataHub.Core.Databricks.Jobs.Extensions.DependencyInjection;
 using Energinet.DataHub.RevisionLog.Integration.Extensions.DependencyInjection;
 using Energinet.DataHub.SettlementReport.Common.Infrastructure.Security;
 using Energinet.DataHub.SettlementReport.Common.Infrastructure.Telemetry;
 using Energinet.DataHub.SettlementReport.Infrastructure.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SettlementReports.WebAPI.Extensions.DependencyInjection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -31,12 +33,13 @@ var host = new HostBuilder()
 
         // Shared by modules
         services.AddNodaTimeForApplication();
+        services.AddDatabricksJobs(context.Configuration);
 
         // revision log
         services.AddRevisionLogIntegrationModule(context.Configuration);
 
         // Modules
-        services.AddSettlementReportsV2Module(context.Configuration);
+        services.AddSettlementReportApiModule(context.Configuration);
     })
     .ConfigureLogging((hostingContext, logging) =>
     {
