7b86f90f98b1dea4cde8c694dbb78819d87c15ca
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-11-01 08:36:46 +0100

    fixing registrations (#57)
    * fixing registrations
    
    * Update injection module

--- source/settlement-report/SettlementReports.Function/Program.cs
+++ source/settlement-report/SettlementReports.Function/Program.cs
@@ -15,11 +15,14 @@
 using Energinet.DataHub.Core.App.Common.Extensions.DependencyInjection;
 using Energinet.DataHub.Core.App.FunctionApp.Extensions.Builder;
 using Energinet.DataHub.Core.App.FunctionApp.Extensions.DependencyInjection;
+using Energinet.DataHub.Core.Databricks.Jobs.Extensions.DependencyInjection;
 using Energinet.DataHub.RevisionLog.Integration.Extensions.DependencyInjection;
 using Energinet.DataHub.SettlementReport.Common.Infrastructure.Security;
 using Energinet.DataHub.SettlementReport.Common.Infrastructure.Telemetry;
 using Energinet.DataHub.SettlementReport.Infrastructure.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SettlementReports.Function.Extensions.DependencyInjection;
+using SettlementReports.WebAPI.Extensions.DependencyInjection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -31,12 +34,13 @@ var host = new HostBuilder()
 
         // Shared by modules
         services.AddNodaTimeForApplication();
+        services.AddDatabricksJobs(context.Configuration);
 
         // revision log
         services.AddRevisionLogIntegrationModule(context.Configuration);
 
         // Modules
-        services.AddSettlementReportsV2Module(context.Configuration);
+        services.AddSettlementReportFunctionModule(context.Configuration);
     })
     .ConfigureLogging((hostingContext, logging) =>
     {
