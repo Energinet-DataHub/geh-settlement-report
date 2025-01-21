6c7511ae4375627619903f9afc1932d52cb7c600
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-01-21 10:58:34 +0100

    Update 202501211010_Update_Previous_Gridarea_Owner.sql

--- source/settlement-report/DatabaseMigration/Scripts/202501211010_Update_Previous_Gridarea_Owner.sql
+++ source/settlement-report/DatabaseMigration/Scripts/202501211010_Update_Previous_Gridarea_Owner.sql
@@ -39,7 +39,7 @@ GO
 --This will add the new owner of the grid area 755, which is valid from the 3rd of January 2022
 INSERT INTO [settlementreports].[GridAreaOwner] (Code, ActorNumber, ValidFrom, SequenceNumber)
 VALUES
-    ('398', '5790001089030', '2024-01-02T00:00:00.0000000+00:00', (SELECT max(SequenceNumber)+1 from  [settlementreports].[GridAreaOwner]))
+    ('398', '5790001089030', '2024-02-01T00:00:00.0000000+00:00', (SELECT max(SequenceNumber)+1 from  [settlementreports].[GridAreaOwner]))
 
 --This is the previous entry we have, which is valid from the 1st of January 2020, which is the earliest date we have in the database
 UPDATE [settlementreports].[GridAreaOwner]
