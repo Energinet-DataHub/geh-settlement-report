6aa47e2178ce3eea0eff81102ee39ecc0204bcca
mrbangbricks <christian.bang@twoday.com>
2025-03-07 08:30:25 +0100

    Introduce new logging setup from python packages version 5.1.2 (#96)
    Co-authored-by: chba <chba@kapacity.dk>
    Co-authored-by: Thomas Erland Clausen <twotcl@inox.dk>

--- source/settlement_report_python/settlement_report_job/entry_points/entry_point.py
+++ source/settlement_report_python/settlement_report_job/entry_points/entry_point.py
@@ -12,26 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+
 import sys
-from argparse import Namespace
-from collections.abc import Callable
 
-from opentelemetry.trace import SpanKind
-
-import geh_common.telemetry.logging_configuration as config
-from geh_common.telemetry.span_recording import span_record_exception
+from geh_common.telemetry.decorators import start_trace
+from geh_common.telemetry import Logger
+from geh_common.telemetry.logging_configuration import (
+    LoggingSettings,
+    configure_logging,
+    add_extras,
+)
 from settlement_report_job.entry_points.tasks import task_factory
 from settlement_report_job.entry_points.job_args.settlement_report_args import (
     SettlementReportArgs,
 )
-from settlement_report_job.entry_points.job_args.settlement_report_job_args import (
-    parse_job_arguments,
-    parse_command_line_arguments,
-)
+
 from settlement_report_job.entry_points.tasks.task_type import TaskType
-from settlement_report_job.entry_points.utils.get_dbutils import get_dbutils
-from settlement_report_job.infrastructure.spark_initializor import initialize_spark
+from settlement_report_job.infrastructure.spark_initializor import (
+    initialize_spark,
+)
 
 
 # The start_x() methods should only have its name updated in correspondence with the
@@ -74,58 +73,37 @@ def start_zip() -> None:
 
 
 def _start_task(task_type: TaskType) -> None:
-    applicationinsights_connection_string = os.getenv(
-        "APPLICATIONINSIGHTS_CONNECTION_STRING"
+    logging_settings = LoggingSettings(
+        subsystem="settlement-report-aggregations",
+        cloud_role_name="dbr-settlement-report",
     )
-
-    start_task_with_deps(
-        task_type=task_type,
-        applicationinsights_connection_string=applicationinsights_connection_string,
-    )
+    configure_logging(logging_settings=logging_settings)
+    start_task_with_deps(task_type=task_type)
 
 
-def start_task_with_deps(
-    *,
-    task_type: TaskType,
-    cloud_role_name: str = "dbr-settlement-report",
-    applicationinsights_connection_string: str | None = None,
-    parse_command_line_args: Callable[..., Namespace] = parse_command_line_arguments,
-    parse_job_args: Callable[..., SettlementReportArgs] = parse_job_arguments,
-) -> None:
-    """Start overload with explicit dependencies for easier testing."""
-    config.configure_logging(
-        cloud_role_name=cloud_role_name,
-        tracer_name="settlement-report-job",
-        applicationinsights_connection_string=applicationinsights_connection_string,
-        extras={"Subsystem": "settlement-report-aggregations"},
-    )
+@start_trace()
+def start_task_with_deps(task_type: TaskType) -> None:
+    add_extras(
+        {"settlement_report_id": get_report_id_from_args()}
+    )  # Add extra before pydantic validation
+    args = SettlementReportArgs()
+    logger = Logger(__name__)
+    logger.info(f"Command line arguments: {args}")
+    spark = initialize_spark()
+    task = task_factory.create(task_type, spark, args)
+    task.execute()
 
-    with config.get_tracer().start_as_current_span(
-        __name__, kind=SpanKind.SERVER
-    ) as span:
-        # Try/except added to enable adding custom fields to the exception as
-        # the span attributes do not appear to be included in the exception.
-        try:
-            # The command line arguments are parsed to have necessary information for
-            # coming log messages
-            command_line_args = parse_command_line_args()
 
-            # Add settlement_report_id to structured logging data to be included in
-            # every log message.
-            config.add_extras({"settlement_report_id": command_line_args.report_id})
-            span.set_attributes(config.get_extras())
-            args = parse_job_args(command_line_args)
-            spark = initialize_spark()
+def get_report_id_from_args() -> str | None:
+    """Checks if --report-id is part of sys.argv and returns its value.
 
-            task = task_factory.create(task_type, spark, args)
-            task.execute()
-
-        # Added as ConfigArgParse uses sys.exit() rather than raising exceptions
-        except SystemExit as e:
-            if e.code != 0:
-                span_record_exception(e, span)
-            sys.exit(e.code)
-
-        except Exception as e:
-            span_record_exception(e, span)
-            sys.exit(4)
+    Returns:
+        str: The value of --report-id if found, otherwise None.
+    """
+    try:
+        # Find the index of --report-id and return the next element
+        index = sys.argv.index("--report-id") + 1
+        return sys.argv[index] if index < len(sys.argv) else None
+    except ValueError:
+        # If --report-id is not found, return None
+        return None
--- source/settlement_report_python/tests/entry_points/job_args/test_settlement_report_args.py
+++ source/settlement_report_python/tests/entry_points/job_args/test_settlement_report_args.py
@@ -13,21 +13,21 @@
 # limitations under the License.
 import re
 import uuid
-from datetime import datetime
+import sys
+from datetime import datetime, timezone
 from unittest.mock import patch
 
+import pydantic
 import pytest
 
 from settlement_report_job.domain.utils.market_role import MarketRole
-from settlement_report_job.entry_points.entry_point import (
-    parse_job_arguments,
-    parse_command_line_arguments,
-)
-
 from settlement_report_job.entry_points.job_args.environment_variables import (
     EnvironmentVariable,
 )
 from settlement_report_job.entry_points.job_args.calculation_type import CalculationType
+from settlement_report_job.entry_points.job_args.settlement_report_args import (
+    SettlementReportArgs,
+)
 
 DEFAULT_REPORT_ID = "12345678-9fc8-409a-a169-fbd49479d718"
 
@@ -63,7 +63,7 @@ def _substitute_energy_supplier_ids(
 ) -> list[str]:
     for i, item in enumerate(sys_argv):
         if item.startswith("--energy-supplier-ids="):
-            sys_argv[i] = f"--energy-supplier-ids={energy_supplier_ids}"
+            sys_argv[i] = f"--energy-supplier-ids={energy_supplier_ids}"  # noqa
             break
     return sys_argv
 
@@ -107,23 +107,26 @@ def job_environment_variables() -> dict:
     }
 
 
+@pytest.fixture(scope="session")
+def timezone_fixture() -> timezone:
+    return timezone.utc
+
+
 def test_when_invoked_with_incorrect_parameters__fails(
     job_environment_variables: dict,
 ) -> None:
     # Arrange
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(pydantic.ValidationError):
         with patch("sys.argv", ["dummy_script", "--unexpected-arg"]):
             with patch.dict("os.environ", job_environment_variables):
                 # Act
-                parse_command_line_arguments()
-
-    # Assert
-    assert excinfo.value.code == 2
+                SettlementReportArgs()
 
 
 def test_when_parameters_for_balance_fixing__parses_parameters_from_contract(
     job_environment_variables: dict,
     sys_argv_from_contract_for_balance_fixing: list[str],
+    timezone_fixture,
 ) -> None:
     """
     This test ensures that the settlement report job for balance fixing accepts
@@ -132,14 +135,14 @@ def test_when_parameters_for_balance_fixing__parses_parameters_from_contract(
     # Arrange
     with patch("sys.argv", sys_argv_from_contract_for_balance_fixing):
         with patch.dict("os.environ", job_environment_variables):
-            command_line_args = parse_command_line_arguments()
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
+            actual_args = SettlementReportArgs()
 
     # Assert - settlement report arguments
     assert actual_args.report_id == DEFAULT_REPORT_ID
-    assert actual_args.period_start == datetime(2022, 5, 31, 22)
-    assert actual_args.period_end == datetime(2022, 6, 1, 22)
+    assert actual_args.period_start == datetime(
+        2022, 5, 31, 22, tzinfo=timezone_fixture
+    )
+    assert actual_args.period_end == datetime(2022, 6, 1, 22, tzinfo=timezone_fixture)
     assert actual_args.calculation_type == CalculationType.BALANCE_FIXING
     assert actual_args.grid_area_codes == ["804", "805"]
     assert actual_args.energy_supplier_ids == ["1234567890123"]
@@ -152,22 +155,24 @@ def test_when_parameters_for_balance_fixing__parses_parameters_from_contract(
 def test_when_parameters_for_wholesale__parses_parameters_from_contract(
     job_environment_variables: dict,
     sys_argv_from_contract_for_wholesale: list[str],
+    timezone_fixture,
 ) -> None:
     """
     This test ensures that the settlement report job for wholesale calculations accepts
     the arguments that are provided by the client.
     """
+
     # Arrange
     with patch("sys.argv", sys_argv_from_contract_for_wholesale):
         with patch.dict("os.environ", job_environment_variables):
-            command_line_args = parse_command_line_arguments()
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
+            actual_args = SettlementReportArgs()
 
     # Assert - settlement report arguments
     assert actual_args.report_id == DEFAULT_REPORT_ID
-    assert actual_args.period_start == datetime(2022, 5, 31, 22)
-    assert actual_args.period_end == datetime(2022, 6, 1, 22)
+    assert actual_args.period_start == datetime(
+        2022, 5, 31, 22, tzinfo=timezone_fixture
+    )
+    assert actual_args.period_end == datetime(2022, 6, 1, 22, tzinfo=timezone_fixture)
     assert actual_args.calculation_type == CalculationType.WHOLESALE_FIXING
     assert actual_args.calculation_id_by_grid_area == {
         "804": uuid.UUID("95bd2365-c09b-4ee7-8c25-8dd56b564811"),
@@ -209,13 +214,11 @@ def test_when_no_valid_calculation_id_for_grid_area__raises_uuid_value_error(
 
     with patch("sys.argv", test_sys_args):
         with patch.dict("os.environ", job_environment_variables):
-            with pytest.raises(ValueError) as exc_info:
-                command_line_args = parse_command_line_arguments()
-                # Act
-                parse_job_arguments(command_line_args)
+            with pytest.raises(pydantic.ValidationError) as exc_info:
+                SettlementReportArgs()
 
     # Assert
-    assert "Calculation ID for grid area 804 is not a uuid" in str(exc_info.value)
+    assert "Input should be a valid UUID" in str(exc_info.value), str(exc_info.value)
 
 
 @pytest.mark.parametrize(
@@ -241,10 +244,7 @@ def test_returns_expected_value_for_prevent_large_text_files(
 
     with patch("sys.argv", test_sys_args):
         with patch.dict("os.environ", job_environment_variables):
-            command_line_args = parse_command_line_arguments()
-
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
+            actual_args = SettlementReportArgs()
 
     # Assert
     assert actual_args.prevent_large_text_files is prevent_large_text_files
@@ -273,10 +273,7 @@ def test_returns_expected_value_for_split_report_by_grid_area(
 
     with patch("sys.argv", test_sys_args):
         with patch.dict("os.environ", job_environment_variables):
-            command_line_args = parse_command_line_arguments()
-
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
+            actual_args = SettlementReportArgs()
 
     # Assert
     assert actual_args.split_report_by_grid_area is split_report_by_grid_area
@@ -305,10 +302,7 @@ def test_returns_expected_value_for_include_basis_data(
 
     with patch("sys.argv", test_sys_args):
         with patch.dict("os.environ", job_environment_variables):
-            command_line_args = parse_command_line_arguments()
-
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
+            actual_args = SettlementReportArgs()
 
     # Assert
     assert actual_args.include_basis_data is include_basis_data
@@ -335,13 +329,9 @@ def test_when_energy_supplier_ids_are_specified__returns_expected_energy_supplie
     test_sys_args = _substitute_energy_supplier_ids(
         test_sys_args, energy_supplier_ids_argument
     )
-
     with patch.dict("os.environ", job_environment_variables):
         with patch("sys.argv", test_sys_args):
-            command_line_args = parse_command_line_arguments()
-
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
+            actual_args = SettlementReportArgs()
 
     # Assert
     assert actual_args.energy_supplier_ids == expected_energy_suppliers_ids
@@ -350,8 +340,6 @@ def test_when_energy_supplier_ids_are_specified__returns_expected_energy_supplie
 @pytest.mark.parametrize(
     "energy_supplier_ids_argument",
     [
-        "1234567890123",  # not a list
-        "1234567890123 2345678901234",  # not a list
         "[123]",  # neither 13 nor 16 characters
         "[12345678901234]",  # neither 13 nor 16 characters
     ],
@@ -369,13 +357,8 @@ def test_when_invalid_energy_supplier_ids__raise_exception(
 
     with patch.dict("os.environ", job_environment_variables):
         with patch("sys.argv", test_sys_args):
-            with pytest.raises(SystemExit) as error:
-                command_line_args = parse_command_line_arguments()
-                # Act
-                parse_job_arguments(command_line_args)
-
-    # Assert
-    assert error.value.code != 0
+            with pytest.raises(pydantic.ValidationError):
+                SettlementReportArgs()
 
 
 def test_when_no_energy_supplier_specified__returns_none_energy_supplier_ids(
@@ -391,11 +374,7 @@ def test_when_no_energy_supplier_specified__returns_none_energy_supplier_ids(
 
     with patch.dict("os.environ", job_environment_variables):
         with patch("sys.argv", test_sys_args):
-            command_line_args = parse_command_line_arguments()
-
-            # Act
-            actual_args = parse_job_arguments(command_line_args)
-
+            actual_args = SettlementReportArgs()
     # Assert
     assert actual_args.energy_supplier_ids is None
 
@@ -418,10 +397,7 @@ class TestWhenInvokedWithValidMarketRole:
 
         with patch("sys.argv", test_sys_args):
             with patch.dict("os.environ", job_environment_variables):
-                command_line_args = parse_command_line_arguments()
-
-                # Act
-                actual_args = parse_job_arguments(command_line_args)
+                actual_args = SettlementReportArgs()
 
         # Assert
         assert actual_args.requesting_actor_market_role == market_role
@@ -441,13 +417,8 @@ class TestWhenInvokedWithInvalidMarketRole:
 
         with patch("sys.argv", test_sys_args):
             with patch.dict("os.environ", job_environment_variables):
-                with pytest.raises(SystemExit) as error:
-                    command_line_args = parse_command_line_arguments()
-                    # Act
-                    parse_job_arguments(command_line_args)
-
-        # Assert
-        assert error.value.code != 0
+                with pytest.raises(pydantic.ValidationError):
+                    SettlementReportArgs()
 
 
 class TestWhenUnknownCalculationType:
@@ -470,13 +441,8 @@ class TestWhenUnknownCalculationType:
 
         with patch("sys.argv", test_sys_args):
             with patch.dict("os.environ", job_environment_variables):
-                with pytest.raises(SystemExit) as error:
-                    command_line_args = parse_command_line_arguments()
-                    # Act
-                    parse_job_arguments(command_line_args)
-
-        # Assert
-        assert error.value.code != 0
+                with pytest.raises(pydantic.ValidationError):
+                    SettlementReportArgs()
 
 
 class TestWhenMissingEnvVariables:
@@ -495,9 +461,7 @@ class TestWhenMissingEnvVariables:
                 }
 
                 with patch.dict("os.environ", env_variables_with_one_missing):
-                    with pytest.raises(ValueError) as error:
-                        command_line_args = parse_command_line_arguments()
-                        # Act
-                        parse_job_arguments(command_line_args)
-
-                assert str(error.value).startswith("Environment variable not found")
+                    with pytest.raises(pydantic.ValidationError) as error:
+                        SettlementReportArgs()
+                assert "Field required " in str(error.value)
+                assert "catalog_name" in str(error.value)
