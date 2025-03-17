7c41abe6bedbb544791759f6c3a15c7cf8fd8780
Jens Peder Meldgaard <jpm@halfspace.io>
2025-03-17 15:26:27 +0100

    ci: Align Python CI/CD across repos (#92)
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    Co-authored-by: Jens Peder Meldgaard <jenspederm@gmail.com>
    Co-authored-by: BjarkeMeier <BjarkeMeier@users.noreply.github.com>

---
+++ source/geh_settlement_report/src/geh_settlement_report/domain/metering_point_periods/order_by_columns.py
@@ -0,0 +1,20 @@
+from geh_settlement_report.domain.utils.csv_column_names import CsvColumnNames
+from geh_settlement_report.domain.utils.market_role import MarketRole
+
+
+def order_by_columns(
+    requesting_actor_market_role: MarketRole,
+) -> list[str]:
+    order_by_column_names = [
+        CsvColumnNames.grid_area_code_in_metering_points_csv,
+        CsvColumnNames.metering_point_type,
+        CsvColumnNames.settlement_method,
+        CsvColumnNames.metering_point_from_date,
+    ]
+    if requesting_actor_market_role in [
+        MarketRole.SYSTEM_OPERATOR,
+        MarketRole.DATAHUB_ADMINISTRATOR,
+    ]:
+        order_by_column_names.insert(1, CsvColumnNames.energy_supplier_id)
+
+    return order_by_column_names
