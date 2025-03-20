6ae483c3a8ec0f86d1f9dd7f5d2cb852c6146246
Soren <XSSFR@energinet.dk>
2025-03-20 08:59:23 +0000

    changed Results and Basis database name to env variables

--- source/geh_settlement_report/src/geh_settlement_report/infrastructure/wholesale/database_definitions.py
+++ source/geh_settlement_report/src/geh_settlement_report/infrastructure/wholesale/database_definitions.py
@@ -11,24 +11,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import ClassVar
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
-class WholesaleBasisDataDatabase:
-    DATABASE_NAME = "wholesale_basis_data"
-    METERING_POINT_PERIODS_VIEW_NAME = "metering_point_periods_v1"
-    TIME_SERIES_POINTS_VIEW_NAME = "time_series_points_v1"
-    CHARGE_PRICE_POINTS_VIEW_NAME = "charge_price_points_v1"
-    CHARGE_LINK_PERIODS_VIEW_NAME = "charge_link_periods_v1"
-    CHARGE_PRICE_INFORMATION_PERIODS_VIEW_NAME = "charge_price_information_periods_v1"
+class WholesaleBasisDataDatabase(BaseSettings):
+    # "wholesale_basis_data"
+    DATABASE_NAME_WHOLESALE_BASIS: str = Field(init=False)
+    METERING_POINT_PERIODS_VIEW_NAME: ClassVar[str] = "metering_point_periods_v1"
+    TIME_SERIES_POINTS_VIEW_NAME: ClassVar[str] = "time_series_points_v1"
+    CHARGE_PRICE_POINTS_VIEW_NAME: ClassVar[str] = "charge_price_points_v1"
+    CHARGE_LINK_PERIODS_VIEW_NAME: ClassVar[str] = "charge_link_periods_v1"
+    CHARGE_PRICE_INFORMATION_PERIODS_VIEW_NAME: ClassVar[str] = "charge_price_information_periods_v1"
 
 
-class WholesaleResultsDatabase:
-    DATABASE_NAME = "wholesale_results"
-    LATEST_CALCULATIONS_BY_DAY_VIEW_NAME = "latest_calculations_by_day_v1"
-    ENERGY_V1_VIEW_NAME = "energy_v1"
-    ENERGY_PER_ES_V1_VIEW_NAME = "energy_per_es_v1"
-    AMOUNTS_PER_CHARGE_VIEW_NAME = (
+class WholesaleResultsDatabase(BaseSettings):
+    #  wholesale_results
+    DATABASE_NAME_WHOLESALE_RESULTS: str = Field(init=False)
+    LATEST_CALCULATIONS_BY_DAY_VIEW_NAME: ClassVar[str] = "latest_calculations_by_day_v1"
+    ENERGY_V1_VIEW_NAME: ClassVar[str] = "energy_v1"
+    ENERGY_PER_ES_V1_VIEW_NAME: ClassVar[str] = "energy_per_es_v1"
+    AMOUNTS_PER_CHARGE_VIEW_NAME: ClassVar[str] = (
         "amounts_per_charge_v1"  # for some reason we call amounts per charge for wholesale results
     )
-    MONTHLY_AMOUNTS_PER_CHARGE_VIEW_NAME = "monthly_amounts_per_charge_v1"
-    TOTAL_MONTHLY_AMOUNTS_VIEW_NAME = "total_monthly_amounts_v1"
+    MONTHLY_AMOUNTS_PER_CHARGE_VIEW_NAME: ClassVar[str] = "monthly_amounts_per_charge_v1"
+    TOTAL_MONTHLY_AMOUNTS_VIEW_NAME: ClassVar[str] = "total_monthly_amounts_v1"
