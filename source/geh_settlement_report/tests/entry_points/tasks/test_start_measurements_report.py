1c3ad08f0c6317627a2c1810f4846e674911d855
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-20 10:14:03 +0200

    chore: use common mock for dbutils
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_settlement_report/tests/entry_points/tasks/test_start_measurements_report.py
+++ source/geh_settlement_report/tests/entry_points/tasks/test_start_measurements_report.py
@@ -1,39 +1,7 @@
-import shutil
-from pathlib import Path
-
 import pytest
+from geh_common.testing.spark.mocks import MockDBUtils
 
 from geh_settlement_report.entry_points.entry_point import start_measurements_report
-
-
-def _remove_path_prefix(path: str) -> str:
-    """Remove the prefix from the path if it exists."""
-    path, *rest = str(path).split(":", 1)
-    if len(rest) > 0:
-        return Path("".join(rest))
-    else:
-        return Path(path)
-
-
-class MockDBUtils:
-    @property
-    def fs(self):
-        class MockFS:
-            def ls(self, path):
-                return [f for f in Path(path).iterdir()]
-
-            def mv(self, src: str | Path, dst: str | Path):
-                src = _remove_path_prefix(src)
-                dst = _remove_path_prefix(dst)
-                shutil.move(Path(src), Path(dst))
-
-            def cp(self, src, dst):
-                if Path(src).is_dir():
-                    shutil.copytree(src, dst)
-                else:
-                    shutil.copy(src, dst)
-
-        return MockFS()
 
 
 @pytest.fixture
