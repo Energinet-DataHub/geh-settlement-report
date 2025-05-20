418431631d6fa5d339978b5532cac4a035975c0e
Bjarke Meier <hej@bjarkemeier.dk>
2025-05-20 09:44:35 +0200

    feat: Add Measurements Report API with request DTOs

---
+++ source/settlement-report/SettlementReports.Interfaces/SettlementReports_v2/Models/MeasurementsReportRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Energinet.DataHub.SettlementReport.Interfaces.SettlementReports_v2.Models;
+
+public sealed record MeasurementsReportRequestDto(
+    MeasurementsReportRequestFilterDto Filter,
+    string? ActorNumberOverride,
+    MarketRole? MarketRoleOverride);
---
+++ source/settlement-report/SettlementReports.Interfaces/SettlementReports_v2/Models/MeasurementsReportRequestFilterDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Energinet.DataHub.SettlementReport.Interfaces.SettlementReports_v2.Models;
+
+public sealed record MeasurementsReportRequestFilterDto(
+    IEnumerable<string> GridAreas,
+    DateTimeOffset PeriodStart,
+    DateTimeOffset PeriodEnd);
