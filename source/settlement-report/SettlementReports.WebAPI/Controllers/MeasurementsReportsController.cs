d0401a15d1f41304fcde85e39425105934049853
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2025-05-20 10:06:53 +0200

    feat: Add Measurements Report API with request DTOs (#120)

---
+++ source/settlement-report/SettlementReports.WebAPI/Controllers/MeasurementsReportsController.cs
@@ -0,0 +1,19 @@
+﻿using Energinet.DataHub.RevisionLog.Integration.WebApi;
+using Energinet.DataHub.SettlementReport.Interfaces.SettlementReports_v2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SettlementReports.WebAPI.Controllers;
+
+[ApiController]
+[Route("measurements-reports")]
+public class MeasurementsReportsController
+    : ControllerBase
+{
+    [HttpPost]
+    [Route("RequestMeasurementsReport")]
+    [EnableRevision(activityName: "RequestMeasurementsReportAPI", entityType: typeof(MeasurementsReportRequestDto))]
+    public ActionResult<long> RequestMeasurementsReport([FromBody] MeasurementsReportRequestDto measurementsReportRequest)
+    {
+        return Ok(42);
+    }
+}
