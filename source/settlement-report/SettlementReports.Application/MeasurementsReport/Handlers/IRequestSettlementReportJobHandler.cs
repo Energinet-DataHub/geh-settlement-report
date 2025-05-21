669061c934c829a302ab876c22c6d8ac59e9a5d7
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2025-05-21 06:01:17 -0700

    Update source/settlement-report/SettlementReports.Application/MeasurementsReport/Handlers/IRequestSettlementReportJobHandler.cs
    Co-authored-by: JesperGomes <112619721+JesperGomes@users.noreply.github.com>

--- source/settlement-report/SettlementReports.Application/MeasurementsReport/Handlers/IRequestSettlementReportJobHandler.cs
+++ source/settlement-report/SettlementReports.Application/MeasurementsReport/Handlers/IRequestSettlementReportJobHandler.cs
@@ -7,7 +7,7 @@ namespace Energinet.DataHub.SettlementReport.Application.MeasurementsReport.Hand
 public interface IRequestMeasurementsReportJobHandler
 {
     /// <summary>
-    /// Request a settlement report job
+    /// Request a measurements report job
     /// </summary>
     /// <param name="request">An object containing the parameters of for the report request</param>
     /// <returns>A JobRunId value representing the run id of the requested settlement report.</returns>
