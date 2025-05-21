a8aadb13fd002f6babcfb854d349027c33b7b3f1
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2025-05-21 06:01:29 -0700

    Update source/settlement-report/SettlementReports.Application/MeasurementsReport/Handlers/IRequestSettlementReportJobHandler.cs
    Co-authored-by: JesperGomes <112619721+JesperGomes@users.noreply.github.com>

--- source/settlement-report/SettlementReports.Application/MeasurementsReport/Handlers/IRequestSettlementReportJobHandler.cs
+++ source/settlement-report/SettlementReports.Application/MeasurementsReport/Handlers/IRequestSettlementReportJobHandler.cs
@@ -10,6 +10,6 @@ public interface IRequestMeasurementsReportJobHandler
     /// Request a measurements report job
     /// </summary>
     /// <param name="request">An object containing the parameters of for the report request</param>
-    /// <returns>A JobRunId value representing the run id of the requested settlement report.</returns>
+    /// <returns>A JobRunId value representing the run id of the requested measurements report.</returns>
     Task<JobRunId> HandleAsync(RequestMeasurementsReportCommand request);
 }
