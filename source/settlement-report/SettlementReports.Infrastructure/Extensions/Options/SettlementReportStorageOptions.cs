7ee17ea027c25c68eeecc50197a75f4e463bda3a
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-05-21 16:54:44 +0200

    removed unused options

--- source/settlement-report/SettlementReports.Infrastructure/Extensions/Options/SettlementReportStorageOptions.cs
+++ source/settlement-report/SettlementReports.Infrastructure/Extensions/Options/SettlementReportStorageOptions.cs
@@ -21,12 +21,6 @@ public class SettlementReportStorageOptions
     public const string SectionName = "SettlementReportStorage";
 
     [Required]
-    public Uri StorageAccountUri { get; set; } = null!;
-
-    [Required]
-    public string StorageContainerName { get; set; } = string.Empty;
-
-    [Required]
     public Uri StorageAccountForJobsUri { get; set; } = null!;
 
     [Required]
