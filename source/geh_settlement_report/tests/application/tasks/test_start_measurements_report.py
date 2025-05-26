1e6547a5e67022c2b6adb3d7711bad57f89108f3
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-26 15:40:35 +0200

    chore: actually check that zip is created
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_settlement_report/tests/application/tasks/test_start_measurements_report.py
+++ source/geh_settlement_report/tests/application/tasks/test_start_measurements_report.py
@@ -4,47 +4,41 @@ import uuid
 import pytest
 from geh_common.testing.spark.mocks import MockDBUtils
 
-from geh_settlement_report.measurements_reports.application.job_args.measurements_report_args import (
-    MeasurementsReportArgs,
-)
 from geh_settlement_report.measurements_reports.entry_point import start_measurements_report
 
 
-@pytest.fixture
-def mock_dbutils(monkeypatch):
+def test_start_measurements_report(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path_factory: pytest.TempPathFactory,
+    dummy_logging: None,
+):
+    # Arrange
+    report_id = uuid.uuid4().hex
+    output_path = tmp_path_factory.mktemp("measurements_report_output")
     monkeypatch.setattr(
         "geh_settlement_report.measurements_reports.application.tasks.measurements_report_task.get_dbutils",
         lambda _: MockDBUtils(),
     )
-    return MockDBUtils()
-
-
-@pytest.fixture
-def default_measurements_report_args(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr(
         sys,
         "argv",
         [
             "entry_point.py",
-            f"--report-id={uuid.uuid4().hex}",
+            f"--report-id={report_id}",
             "--period-start=2025-01-01",
             "--period-end=2025-01-31",
             "--grid-area-codes=123,456",
         ],
     )
-    monkeypatch.setenv("OUTPUT_PATH", "/tmp")
+    monkeypatch.setenv("OUTPUT_PATH", str(output_path))
     monkeypatch.setenv("CATALOG_NAME", "spark_catalog")
     monkeypatch.setenv("TIME_ZONE", "Europe/Copenhagen")
     monkeypatch.setenv(
         "APPLICATIONINSIGHTS_CONNECTION_STRING", "InstrumentationKey=12345678-1234-1234-1234-123456789012"
     )
 
-    return MeasurementsReportArgs()
-
-
-def test_start_measurements_report(
-    default_measurements_report_args,
-    mock_dbutils,
-    dummy_logging: None,
-):
+    # Act
     start_measurements_report()
+
+    # Assert
+    assert (output_path / report_id).with_suffix(".zip").exists(), "Report CSV file was not created"
