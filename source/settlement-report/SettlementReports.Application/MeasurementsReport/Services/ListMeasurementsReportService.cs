bb3a13a4a4ec1746739f38aa35a34cbb34d17c4e
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2025-05-27 09:11:41 +0200

    feat: Add ListMeasurementsReportService (#147)

---
+++ source/settlement-report/SettlementReports.Application/MeasurementsReport/Services/ListMeasurementsReportService.cs
@@ -0,0 +1,16 @@
+﻿using Energinet.DataHub.SettlementReport.Interfaces.SettlementReports_v2.Models.MeasurementsReport;
+
+namespace Energinet.DataHub.SettlementReport.Application.MeasurementsReport.Services;
+
+public sealed class ListMeasurementsReportService : IListMeasurementsReportService
+{
+    public Task<IEnumerable<RequestedMeasurementsReportDto>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<RequestedMeasurementsReportDto>> GetAsync(Guid actorId)
+    {
+        throw new NotImplementedException();
+    }
+}
