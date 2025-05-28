5e88cc48c9e564d408755880236a428d504820f1
Alexander <alexanderharrow@hotmail.com>
2025-05-28 09:42:39 +0200

    Update 202505271500_Add_MeasurementsReport_table.sql

--- source/settlement-report/DatabaseMigration/Scripts/202505271500_Add_MeasurementsReport_table.sql
+++ source/settlement-report/DatabaseMigration/Scripts/202505271500_Add_MeasurementsReport_table.sql
@@ -1,39 +1,16 @@
-CREATE SCHEMA [settlementreports]
-    GO
-
 CREATE TABLE [settlementreports].[MeasurementsReport]
 (
-    [
-    Id] [
-    int]
-    IDENTITY
-(
-    1,
-    1
-) NOT NULL,
+    [Id] [int] IDENTITY (1, 1) NOT NULL,
     [UserId] [uniqueidentifier] NOT NULL,
     [ActorId] [uniqueidentifier] NOT NULL,
-    [RequestId] [nvarchar]
-(
-    512
-) NOT NULL,
+    [RequestId] [nvarchar] (512) NOT NULL,
     [Status] int NOT NULL,
-    [BlobFilename] [nvarchar]
-(
-    2048
-) NULL,
+    [BlobFilename] [nvarchar] (2048) NULL,
     [PeriodStart] [datetime2] NOT NULL,
     [PeriodEnd] [datetime2] NOT NULL,
     [CreatedDateTime] [datetimeoffset] NOT NULL,
     [JobRunId] [bigint] NULL,
     [EndedDateTime] [datetime2] NULL,
-    [GridAreaCodes] [nvarchar]
-(
-    max
-) NOT NULL,
-    CONSTRAINT [PK_MeasurementsReportRequest] PRIMARY KEY
-(
-[
-    Id]
+    [GridAreaCodes] [nvarchar] (max) NOT NULL,
+    CONSTRAINT [PK_MeasurementsReportRequest] PRIMARY KEY ([Id])
 )
-    )
