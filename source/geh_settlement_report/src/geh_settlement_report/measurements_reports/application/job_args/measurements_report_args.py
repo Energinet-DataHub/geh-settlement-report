df21ab5326e170d7e8868097151661fb5cd580f4
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-30 11:07:01 +0200

    chore: adds type for EnergySupplierIDs
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_settlement_report/src/geh_settlement_report/measurements_reports/application/job_args/measurements_report_args.py
+++ source/geh_settlement_report/src/geh_settlement_report/measurements_reports/application/job_args/measurements_report_args.py
@@ -1,8 +1,44 @@
+import re
 from datetime import datetime
+from typing import Annotated, Any
 
 from geh_common.application import GridAreaCodes
-from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import AfterValidator, BeforeValidator, Field
+from pydantic_settings import BaseSettings, NoDecode, SettingsConfigDict
+
+
+def _str_to_list(value: Any) -> list[str] | None:
+    """Convert the input value to a list of grid area codes (strings).
+
+    Args:
+        value (Any): The input value to convert.
+
+    Returns:
+        Optional[List[str]]: A list of grid area codes or None if the input is empty.
+    """
+    if not value:
+        return None
+    if isinstance(value, list):
+        return [str(item) for item in value]
+    else:
+        return re.findall(r"\d+", value)
+
+
+def _validate_energy_supplier_ids(v: list[str]) -> list[str]:
+    """Validate the list of energy supplier IDs."""
+    if not v:
+        return v
+    for id_ in v:
+        if not isinstance(id_, str):
+            raise TypeError(f"Energy supplier IDs must be strings, not {type(id_)}")
+        if not id_.isdigit():
+            raise ValueError(f"Unexpected energy supplier ID: '{id_}'. Energy supplier IDs must consist of 8 digits.")
+    return v
+
+
+EnergySupplierIds = Annotated[
+    list[str], BeforeValidator(_str_to_list), AfterValidator(_validate_energy_supplier_ids), NoDecode()
+]
 
 
 class MeasurementsReportArgs(BaseSettings):
@@ -20,7 +56,7 @@ class MeasurementsReportArgs(BaseSettings):
     grid_area_codes: GridAreaCodes | None = Field(init=False, default=None)
     requesting_actor_id: str = Field(init=False)
     # TODO BJM: Update to correctly parse input parameter - share functionality with settlement report args
-    energy_supplier_ids: list[str] = Field(init=False, default_factory=list)
+    energy_supplier_ids: EnergySupplierIds = Field(init=False, default_factory=list)
 
     catalog_name: str = Field(init=False)
     output_path: str = Field(init=False)
