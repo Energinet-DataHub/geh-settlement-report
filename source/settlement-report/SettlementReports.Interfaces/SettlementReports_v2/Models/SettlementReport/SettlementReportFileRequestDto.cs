0200920b0b75bc2af7366a70024d864ed05fb9d4
Bjarke Meier <hej@bjarkemeier.dk>
2025-06-02 16:48:36 +0200

    remove unused code

--- source/settlement-report/SettlementReports.Interfaces/SettlementReports_v2/Models/SettlementReport/SettlementReportFileRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Energinet.DataHub.Reports.Interfaces.SettlementReports_v2.Models.SettlementReport;
-
-public sealed record SettlementReportFileRequestDto(
-    ReportRequestId RequestId,
-    SettlementReportFileContent FileContent,
-    SettlementReportPartialFileInfo PartialFileInfo,
-    SettlementReportRequestFilterDto RequestFilter,
-    long MaximumCalculationVersion);
