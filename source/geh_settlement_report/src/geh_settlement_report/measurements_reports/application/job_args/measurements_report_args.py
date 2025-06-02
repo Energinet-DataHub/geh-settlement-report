e0f4bc31f6d600c8e69ba7f5ba38665e907b02ae
Jens Peder Meldgaard <jpm@halfspace.io>
2025-06-02 10:30:59 +0200

    chore: move energy supplier validation to geh_common

--- source/geh_settlement_report/src/geh_settlement_report/measurements_reports/application/job_args/measurements_report_args.py
+++ source/geh_settlement_report/src/geh_settlement_report/measurements_reports/application/job_args/measurements_report_args.py
@@ -1,44 +1,8 @@
-import re
 from datetime import datetime
-from typing import Annotated, Any
 
-from geh_common.application import GridAreaCodes
-from pydantic import AfterValidator, BeforeValidator, Field
-from pydantic_settings import BaseSettings, NoDecode, SettingsConfigDict
-
-
-def _str_to_list(value: Any) -> list[str] | None:
-    """Convert the input value to a list of grid area codes (strings).
-
-    Args:
-        value (Any): The input value to convert.
-
-    Returns:
-        Optional[List[str]]: A list of grid area codes or None if the input is empty.
-    """
-    if not value:
-        return None
-    if isinstance(value, list):
-        return [str(item) for item in value]
-    else:
-        return re.findall(r"\d+", value)
-
-
-def _validate_energy_supplier_ids(v: list[str]) -> list[str]:
-    """Validate the list of energy supplier IDs."""
-    if not v:
-        return v
-    for id_ in v:
-        if not isinstance(id_, str):
-            raise TypeError(f"Energy supplier IDs must be strings, not {type(id_)}")
-        if not id_.isdigit():
-            raise ValueError(f"Unexpected energy supplier ID: '{id_}'.")
-    return v
-
-
-EnergySupplierIds = Annotated[
-    list[str] | None, BeforeValidator(_str_to_list), AfterValidator(_validate_energy_supplier_ids), NoDecode()
-]
+from geh_common.application import EnergySupplierIds, GridAreaCodes
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class MeasurementsReportArgs(BaseSettings):
