28aeb0a0fafcca4466cc14d161531a00c184be48
Alexander <alexanderharrow@hotmail.com>
2025-06-04 19:50:34 +0200

    Update MeasurementsReportFileRepository.cs

--- source/settlement-report/SettlementReports.Infrastructure/Persistence/MeasurementsReport/MeasurementsReportFileRepository.cs
+++ source/settlement-report/SettlementReports.Infrastructure/Persistence/MeasurementsReport/MeasurementsReportFileRepository.cs
@@ -30,6 +30,10 @@ public sealed class MeasurementsReportFileRepository : IMeasurementsReportFileRe
             throw new ArgumentException("Invalid file name.", nameof(fileName));
 
         var blobName = string.Join('/', _options.Value.DirectoryPath, fileName);
+
+        if (string.IsNullOrWhiteSpace(blobName) || blobName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+            throw new ArgumentException("Invalid file name 2.", nameof(fileName));
+
         var blobClient = _blobContainerClient.GetBlobClient(blobName);
         return await blobClient.OpenReadAsync().ConfigureAwait(false);
     }
