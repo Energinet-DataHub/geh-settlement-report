b4239187c96b11ae35e62909b30470499e5c2a50
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-06-10 13:42:33 +0200

    Update MeasurementsReportsController.cs

--- source/settlement-report/SettlementReports.WebAPI/Controllers/MeasurementsReportsController.cs
+++ source/settlement-report/SettlementReports.WebAPI/Controllers/MeasurementsReportsController.cs
@@ -6,6 +6,7 @@ using Energinet.DataHub.Reports.Abstractions.Model.MeasurementsReport;
 using Energinet.DataHub.Reports.Application.MeasurementsReport.Commands;
 using Energinet.DataHub.Reports.Application.MeasurementsReport.Handlers;
 using Energinet.DataHub.Reports.Application.MeasurementsReport.Services;
+using Energinet.DataHub.Reports.Application.Model;
 using Energinet.DataHub.Reports.Common.Infrastructure.Security;
 using Energinet.DataHub.Reports.WebAPI.Controllers.Mappers;
 using Microsoft.AspNetCore.Authorization;
